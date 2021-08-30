dd728df512bb51831f9a487f2d7b9937f8358975
hackandcode <hackandcode@web.de>
2021-08-30 12:37:21 +0200

    first commit

---
+++ just_vscode.sh
@@ -0,0 +1,107 @@
+# Snap install for VSCode
+snap install code --classic
+
+# Vim Keybinding
+code --install-extension vscodevim.vim
+
+# Some cool extensions for everyone
+code --install-extension formulahendry.code-runner
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension 2gua.rainbow-brackets
+code --install-extension 4tron.stack-overflow-view
+code --install-extension alefragnani.project-manager
+code --install-extension christian-kohler.path-intellisense
+code --install-extension usernamehw.errorlens
+
+# Git Extensions
+code --install-extension codezombiech.gitignore
+code --install-extension donjayamanne.git-extension-pack
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension codestream.codestream
+# if you use GitLab
+code --install-extension GitLab.gitlab-workflow
+# if you use GitHub
+code --install-extension github.vscode-pull-request-github
+code --install-extension ziyasal.vscode-open-in-github
+# if you use BitBucket and Jira
+code --install-extension atlassian.atlascode
+
+# Database Tools
+# SQL Based
+code --install-extension mtxr.sqltools
+# Drivers for SQL Databases
+code --install-extension mtxr.sqltools-driver-mssql
+code --install-extension mtxr.sqltools-driver-mysql
+code --install-extension mtxr.sqltools-driver-pg
+code --install-extension mtxr.sqltools-driver-sqlite
+code --install-extension saposs.sap-hana-driver-for-sqltools
+code --install-extension koszti.snowflake-driver-for-sqltools
+code --install-extension ultram4rine.sqltools-clickhouse-driver
+code --install-extension kj.sqltools-driver-redshift
+code --install-extension google-cloud-spanner-ecosystem.google-cloud-spanner-driver
+code --install-extension brunoventura.sqltools-athena-driver
+# Cassandra Driver (requires SQL Databases)
+code --install-extension jordanhury.sqltools-cassandra
+# MongoDB
+code --install-extension mongodb.mongodb-vscode
+
+# Container tools
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+# if you use Kubernetes
+code --install.extension ms-kubernetes-tools.vscode-kubernetes-tools
+
+# Web-Development
+code --install-extension techer.open-in-browser
+code --install-extension ritwickdey.liveserver
+code --install-extension Wscats.eno
+code --install-extension tombonnike.vscode-status-bar-format-toggle
+
+# API Development
+code --install-extension rohinivsenthil.postcode
+
+# SSH Manager
+code --install-extension cweijan.vscode-ssh
+
+# CI Pipline Manager
+code --install-extension jodyh.circleci-vscode
+
+# Microcontroller Development
+code --install-extension ms-vscode.cpptools
+code --install-extension platformio.platformio-ide
+
+# Package Managers
+## Pip
+code --install-extension slightc.pip-manager
+## npm
+code --install-extension dennisvhest.npm-browser
+## NuGet
+code --install-extension jmrog.vscode-nuget-package-manager
+
+# Kanbanboard
+code --install-extension mkloubert.vscode-kanban
+
+# if you want to paint in VSCode
+code --install-extension tyriar.luna-paint
+
+# LateX Support
+code --install-extension ajshort.latex-preview
+
+# Jupyter Notebooks
+code --install-extension ms-python.python
+code --install-extension ms-toolsai.jupyter
+
+# Material Theme for VSCode
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension PKief.material-icon-theme
+code --install-extension zhuangtongfa.material-theme
+
+#this one is just for fun, delete if you want :)
+code --install-extension bar9.stories
+code --install-extension benawad.vsinder
+code --install-extension mkloubert.vs-media-player
+code --install-extension shyykoserhiy.vscode-spotify
+
