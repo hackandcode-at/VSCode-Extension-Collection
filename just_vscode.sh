aebc3c2d803fbaddfe432b8d5d780ec70fa8ed7f
hackandcode <hackandcode@web.de>
2021-08-30 13:15:55 +0200

    added user inputs

--- just_vscode.sh
+++ just_vscode.sh
@@ -1,8 +1,7 @@
+#!/bin/bash
+
 # Snap install for VSCode
 snap install code --classic
-
-# Vim Keybinding
-code --install-extension vscodevim.vim
 
 # Some cool extensions for everyone
 code --install-extension formulahendry.code-runner
@@ -13,95 +12,203 @@ code --install-extension alefragnani.project-manager
 code --install-extension christian-kohler.path-intellisense
 code --install-extension usernamehw.errorlens
 
-# Git Extensions
-code --install-extension codezombiech.gitignore
-code --install-extension donjayamanne.git-extension-pack
-code --install-extension donjayamanne.githistory
-code --install-extension eamodio.gitlens
-code --install-extension codestream.codestream
-# if you use GitLab
-code --install-extension GitLab.gitlab-workflow
-# if you use GitHub
-code --install-extension github.vscode-pull-request-github
-code --install-extension ziyasal.vscode-open-in-github
-# if you use BitBucket and Jira
-code --install-extension atlassian.atlascode
+read -p "Do you want the Vim Layout? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension vscodevim.vim
+    ;;
+esac
 
-# Database Tools
-# SQL Based
-code --install-extension mtxr.sqltools
-# Drivers for SQL Databases
-code --install-extension mtxr.sqltools-driver-mssql
-code --install-extension mtxr.sqltools-driver-mysql
-code --install-extension mtxr.sqltools-driver-pg
-code --install-extension mtxr.sqltools-driver-sqlite
-code --install-extension saposs.sap-hana-driver-for-sqltools
-code --install-extension koszti.snowflake-driver-for-sqltools
-code --install-extension ultram4rine.sqltools-clickhouse-driver
-code --install-extension kj.sqltools-driver-redshift
-code --install-extension google-cloud-spanner-ecosystem.google-cloud-spanner-driver
-code --install-extension brunoventura.sqltools-athena-driver
-# Cassandra Driver (requires SQL Databases)
-code --install-extension jordanhury.sqltools-cassandra
-# MongoDB
-code --install-extension mongodb.mongodb-vscode
+read -p "Do you want Git? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension codezombiech.gitignore
+        code --install-extension donjayamanne.git-extension-pack
+        code --install-extension donjayamanne.githistory
+        code --install-extension eamodio.gitlens
+        code --install-extension codestream.codestreamcode --install-extension vscodevim.vim
+    ;;
+esac
 
-# Container tools
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension ms-vscode-remote.remote-ssh-edit
-# if you use Kubernetes
-code --install.extension ms-kubernetes-tools.vscode-kubernetes-tools
+read -p "Do you use GitLab? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension GitLab.gitlab-workflow
+    ;;
+esac
 
-# Web-Development
-code --install-extension techer.open-in-browser
-code --install-extension ritwickdey.liveserver
-code --install-extension Wscats.eno
-code --install-extension tombonnike.vscode-status-bar-format-toggle
+read -p "Do you use GitHub? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension github.vscode-pull-request-github
+        code --install-extension ziyasal.vscode-open-in-github
+    ;;
+esac
 
-# API Development
-code --install-extension rohinivsenthil.postcode
+read -p "Do you use GitHub? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension atlassian.atlascode
+    ;;
+esac
 
-# SSH Manager
-code --install-extension cweijan.vscode-ssh
+read -p "Do you need SQL? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension mtxr.sqltools
+        code --install-extension mtxr.sqltools-driver-mssql
+        code --install-extension mtxr.sqltools-driver-mysql
+        code --install-extension mtxr.sqltools-driver-pg
+        code --install-extension mtxr.sqltools-driver-sqlite
+        code --install-extension saposs.sap-hana-driver-for-sqltools
+        code --install-extension koszti.snowflake-driver-for-sqltools
+        code --install-extension ultram4rine.sqltools-clickhouse-driver
+        code --install-extension kj.sqltools-driver-redshift
+        code --install-extension google-cloud-spanner-ecosystem.google-cloud-spanner-driver
+        code --install-extension brunoventura.sqltools-athena-drivercode --install-extension atlassian.atlascode
+    ;;
+esac
 
-# CI Pipline Manager
-code --install-extension jodyh.circleci-vscode
+read -p "Do you need Cassandra? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension mtxr.sqltools
+        code --install-extension jordanhury.sqltools-cassandra
+    ;;
+esac
 
-# Microcontroller Development
-code --install-extension ms-vscode.cpptools
-code --install-extension platformio.platformio-ide
+read -p "Do you need MongoDB? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension mongodb.mongodb-vscode
+    ;;
+esac
 
-# Package Managers
-## Pip
-code --install-extension slightc.pip-manager
-## npm
-code --install-extension dennisvhest.npm-browser
-## NuGet
-code --install-extension jmrog.vscode-nuget-package-manager
+read -p "Do you need Redis? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension dunn.redis
+    ;;
+esac
 
-# Kanbanboard
-code --install-extension mkloubert.vscode-kanban
+read -p "Do you need Docker? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension ms-azuretools.vscode-docker
+        code --install-extension ms-vscode-remote.remote-containers
+        code --install-extension ms-vscode-remote.remote-ssh
+        code --install-extension ms-vscode-remote.remote-ssh-edit
+    ;;
+esac
 
-# if you want to paint in VSCode
-code --install-extension tyriar.luna-paint
+read -p "Do you need Kubernetes? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install.extension ms-kubernetes-tools.vscode-kubernetes-tools
+    ;;
+esac
 
-# LateX Support
-code --install-extension ajshort.latex-preview
+read -p "Are you Web-Developer? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension techer.open-in-browser
+        code --install-extension ritwickdey.liveserver
+        code --install-extension Wscats.eno
+        code --install-extension tombonnike.vscode-status-bar-format-toggle
+    ;;
+esac
 
-# Jupyter Notebooks
-code --install-extension ms-python.python
-code --install-extension ms-toolsai.jupyter
+read -p "Are you API-Developer? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension rohinivsenthil.postcode
+    ;;
+esac
 
-# Material Theme for VSCode
-code --install-extension Equinusocio.vsc-material-theme
-code --install-extension PKief.material-icon-theme
-code --install-extension zhuangtongfa.material-theme
+read -p "Do you need an SSH-Manager? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension cweijan.vscode-ssh
+    ;;
+esac
 
-#this one is just for fun, delete if you want :)
-code --install-extension bar9.stories
-code --install-extension benawad.vsinder
-code --install-extension mkloubert.vs-media-player
-code --install-extension shyykoserhiy.vscode-spotify
+read -p "Do you need an CI-Pipline Manager? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension jodyh.circleci-vscode
+    ;;
+esac
+
+read -p "Are you an Hardware Developer? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension ms-vscode.cpptools
+        code --install-extension platformio.platformio-ide
+    ;;
+esac
+
+read -p "Do you need Pip? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension slightc.pip-manager
+    ;;
+esac
+
+read -p "Do you need npm? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension dennisvhest.npm-browser
+    ;;
+esac
+
+read -p "Do you need NuGet? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension jmrog.vscode-nuget-package-manager
+    ;;
+esac
+
+read -p "Do you need Kanban? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension mkloubert.vscode-kanban
+    ;;
+esac
+
+read -p "Are you Data Scientist/AI-Developer? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension ms-python.python
+        code --install-extension ms-toolsai.jupyter
+    ;;
+esac
+
+read -p "Do you need LateX? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension ajshort.latex-preview
+    ;;
+esac
+
+read -p "Do you want the Material Design? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension Equinusocio.vsc-material-theme
+        code --install-extension PKief.material-icon-theme
+        code --install-extension zhuangtongfa.material-theme
+    ;;
+esac
+
+read -p "Do you want some Fun-Extensions? (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        code --install-extension bar9.stories
+        code --install-extension benawad.vsinder
+        code --install-extension mkloubert.vs-media-player
+        code --install-extension shyykoserhiy.vscode-spotify
+        code --install-extension tyriar.luna-paint
+    ;;
+esac
+
+
 
