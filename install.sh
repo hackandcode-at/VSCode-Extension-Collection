ed0728b69152d87dc7cae78cdbc1cff4a016c057
Luis Herr <78873853+AmphoMoronal@users.noreply.github.com>
2021-10-24 13:06:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -168,6 +168,9 @@ case ${answer:0:1} in
         code --install-extension peterhdd.dartgettersetter
         code --install-extension yongzhenlow.dart-built-value-snippets
         code --install-extension yashpaneliya.fast-dart
+        code --install-extension dart-code.flutter
+        code --install-extension alexisvt.flutter-snippets
+        code --install-extension gornivv.vscode-flutter-files
     ;;
 esac
 echo "$(tput setaf 3)##################################################$(tput sgr 0)"
