7a30a722e4d6e998aec91d7b8efc46588f1bd2ff
MarkusS <markusscoding@outlook.com>
2022-01-26 12:02:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -111,6 +111,7 @@ read -p "Are you an API-Developer? (y/n) " answer
 case ${answer:0:1} in
     y|Y )
         code --install-extension rangav.vscode-thunder-client
+        code --install-extension tanhakabir.rest-book
     ;;
 esac
 echo "$(tput setaf 3)##################################################$(tput sgr 0)"
